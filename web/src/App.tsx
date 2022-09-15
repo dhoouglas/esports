3644429caa545f25813879670b0294171264f82b
unknown <dhoouglas@hotmail.com>
2022-09-14 21:06:30 -0300

    update lage Page, include caixa Publicar Anuncio

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,3 +1,5 @@
+import { MagnifyingGlassPlus } from 'phosphor-react';
+
 import './styles/main.css';
 
 import logoImg from './assets/logo-nlw-esports.svg';
@@ -12,7 +14,7 @@ function App() {
       </h1>
 
       <div className="grid grid-cols-6 gap-6 mt-16">
-        <a href="" className="relative rounded-lg overflow-hidden rounded-lg overflow-hidden">
+        <a href="" className="relative rounded-lg overflow-hidden ">
           <img src="/game-1.png" alt="" />
 
           <div className="w-full pt-16 pb-4 px-4 bg-game-gradinet absolute bottom-0 left-0 right-0">
@@ -62,6 +64,22 @@ function App() {
         </a>
       
       </div>
+
+      <div className="pt-1 bg-nlw-gradient self-stretch rounded-lg mt-8 overflow-hidden">
+        <div className="bg-[#2A2634] px-8 py-6 flex justify-between items-center">
+          <div>
+            <strong className="text-2xl text-white font-black block">Não encontrou seu duo ?</strong>
+            <span className="text-zinc-400 block">Publique um anúncio para encontrar novos players !</span>
+          </div>
+
+          <button className="py-3 px-4 bg-violet-500 hover:bg-violet-600 text-white rounded flex items-center gap-3">
+            <MagnifyingGlassPlus size={24}/>
+            Publicar Anúncio
+          </button>
+
+        </div>
+      </div>
+      
     </div>
 
     
--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -5,11 +5,13 @@ module.exports = {
     './index.html',
   ],
   theme: {
+    fontFamily: {
+      'sans': ['Inter', 'sans-serif']     
+    },
     extend: {
-      
       backgroundImage: {
         galaxy: "url('/background-galaxy.png')",
-        'nlw-gradient': 'linear-gradient(89.86deg, #9572FC 40.08%, #43E7AD 70.94%, #E1D55D 5.57%)',
+        'nlw-gradient': 'linear-gradient(89.86deg, #9572FC 20.08%, #43E7AD 70.94%, #E1D55D 90.57%)',
         'game-gradinet': 'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08% )',
       },
     },
