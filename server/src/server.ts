37df98b588a4809c85733f52ef8d0aca788774ef
unknown <dhoouglas@hotmail.com>
2022-09-16 00:10:42 -0300

    Config cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,4 +1,6 @@
 import express, { request } from "express";
+import cors from 'cors'
+
 import { PrismaClient } from '@prisma/client';
 import { convertHourStringToMinutes } from './utils/convert-hour-string-to-minutes';
 import { convertMinutesToHoursString } from "./utils/convert-minutes-to-hour-string";
@@ -7,6 +9,7 @@ import { convertMinutesToHoursString } from "./utils/convert-minutes-to-hour-str
 const app = express();
 
 app.use(express.json())
+app.use(cors())
 
 const prisma = new PrismaClient({
     log: ['query']
