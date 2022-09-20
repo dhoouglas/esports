00ba5b27f24c38e15d67b6a73e44ed815fea73b4
unknown <dhoouglas@hotmail.com>
2022-09-19 21:41:05 -0300

    Mobile - API connection

--- mobile/src/components/GameCard/index.tsx
+++ mobile/src/components/GameCard/index.tsx
@@ -7,9 +7,11 @@ import { THEME } from '../../theme';
 
 export interface GameCardProps {
   id: string;
-  name: string;
-  ads: string;
-  cover: ImageSourcePropType;
+  title: string;
+  _count: {
+    ads: number
+  };
+  bannerUrl: string;
 
 }
 
@@ -22,7 +24,7 @@ export function GameCard({ data, ...rest } : Props) {
     <TouchableOpacity style={styles.container} {...rest}>
       <ImageBackground 
         style={styles.cover}
-        source={data.cover}
+        source={{ uri: data.bannerUrl }}
       >
 
       <LinearGradient
@@ -30,10 +32,10 @@ export function GameCard({ data, ...rest } : Props) {
         style={styles.footer}  
       >
         <Text style={styles.name}>
-          {data.name}
+          {data.title}
         </Text>
         <Text style={styles.ads}>
-          {data.ads} anúncios
+          {data._count.ads} anúncios
         </Text>
         
       </LinearGradient>
--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,16 +1,26 @@
+import { useEffect, useState } from 'react';
 import { View, Image, FlatList } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 import logoImg from '../../assets/logo-nlw-esports.png'
-import { GameCard } from '../../components/GameCard';
-import { Heading } from '../../components/Heading';
 
-import { GAMES } from '../../utils/games';
+import { GameCard, GameCardProps } from '../../components/GameCard';
+import { Heading } from '../../components/Heading';
 
 import { styles } from './styles';
 
 export function Home() {
+  const [games, setGames] = useState<GameCardProps[]>([]);
+
+  useEffect(() =>{
+    fetch('http://192.168.0.106:3333/games')
+    .then(response => response.json())
+    .then(data => setGames(data))
+
+  }, []);
+
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <Image 
         source={logoImg}
         style={styles.logo}
@@ -23,7 +33,7 @@ export function Home() {
       />
 
       <FlatList 
-        data={GAMES}
+        data={games}
         keyExtractor={item => item.id}
         renderItem={({ item }) => (
           <GameCard 
@@ -35,6 +45,6 @@ export function Home() {
         contentContainerStyle={styles.contentList}
       />
 
-    </View>
+    </SafeAreaView>
   );
 }
